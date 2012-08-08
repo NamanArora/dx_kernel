4af5281f4af9c88f2e2b438ae73e57889789945a
Michael Grzeschik <m.grzeschik@pengutronix.de>
2012-08-08 11:48:10 +0200

    usb: gadget: u_ether: fix kworker 100% CPU issue with still used interfaces in eth_stop
    commit b1b552a69b8805e7e338074a9e8b670b4a795218 upstream.
    
    This patch fixes an issue introduced by patch:
    
        72c973d usb: gadget: add usb_endpoint_descriptor to struct usb_ep
    
    Without this patch we see a kworker taking 100% CPU, after this sequence:
    
    - Connect gadget to a windows host
    - load g_ether
    - ifconfig up <ip>; ifconfig down; ifconfig up
    - ping <windows host>
    
    The "ifconfig down" results in calling eth_stop(), which will call
    usb_ep_disable() and, if the carrier is still ok, usb_ep_enable():
    
             usb_ep_disable(link->in_ep);
             usb_ep_disable(link->out_ep);
             if (netif_carrier_ok(net)) {
                     usb_ep_enable(link->in_ep);
                     usb_ep_enable(link->out_ep);
             }
    
    The ep should stay enabled, but will not, as ep_disable set the desc
    pointer to NULL, therefore the subsequent ep_enable will fail. This leads
    to permanent rescheduling of the eth_work() worker as usb_ep_queue()
    (called by the worker) will fail due to the unconfigured endpoint.
    
    We fix this issue by saving the ep descriptors and re-assign them before
    usb_ep_enable().
    
    Cc: Tatyana Brokhman <tlinder@codeaurora.org>
    Signed-off-by: Michael Grzeschik <m.grzeschik@pengutronix.de>
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    Conflicts:
    	drivers/usb/gadget/u_ether.c

--- drivers/usb/gadget/u_ether.c
+++ drivers/usb/gadget/u_ether.c
@@ -736,9 +736,24 @@ static int eth_stop(struct net_device *net)
 	spin_lock_irqsave(&dev->lock, flags);
 	if (dev->port_usb) {
 		struct gether	*link = dev->port_usb;
+		const struct usb_endpoint_descriptor *in;
+		const struct usb_endpoint_descriptor *out;
 
 		if (link->close)
 			link->close(link);
+
+
+		/* NOTE:  we have no abort-queue primitive we could use
+		 * to cancel all pending I/O.  Instead, we disable then
+		 * reenable the endpoints ... this idiom may leave toggle
+		 * wrong, but that's a self-correcting error.
+		 *
+		 * REVISIT:  we *COULD* just let the transfers complete at
+		 * their own pace; the network stack can handle old packets.
+		 * For the moment we leave this here, since it works.
+		 */
+		in = link->in_ep->desc;
+		out = link->out_ep->desc;
 
 		usb_ep_disable(link->in_ep);
 		usb_ep_disable(link->out_ep);
@@ -752,6 +767,8 @@ static int eth_stop(struct net_device *net)
 				return -EINVAL;
 			}
 			DBG(dev, "host still using in/out endpoints\n");
+			link->in_ep->desc = in;
+			link->out_ep->desc = out;
 			usb_ep_enable(link->in_ep);
 			usb_ep_enable(link->out_ep);
 		}
