eeaf0fd315f0c524e3a4fddf395c5ef7aacca7eb
Rachit Rawat <rachit.rc96@gmail.com>
2013-09-21 17:19:36 +0530

    Dynamic Fsync 2.0
    Conflicts:
    	arch/arm/configs/sa77_defconfig
    	fs/sync.c

--- fs/sync.c
+++ fs/sync.c
@@ -14,7 +14,14 @@
 #include <linux/backing-dev.h>
 #include "internal.h"
 
+
 #include <trace/events/mmcio.h>
+
+#ifdef CONFIG_DYNAMIC_FSYNC
+extern bool early_suspend_active;
+extern bool dyn_fsync_active;
+#endif
+
 
 #define VALID_FLAGS (SYNC_FILE_RANGE_WAIT_BEFORE|SYNC_FILE_RANGE_WRITE| \
 			SYNC_FILE_RANGE_WAIT_AFTER)
@@ -131,13 +138,17 @@ SYSCALL_DEFINE1(syncfs, int, fd)
  */
 int vfs_fsync_range(struct file *file, loff_t start, loff_t end, int datasync)
 {
-	int err;
-	if (!file->f_op || !file->f_op->fsync)
-		return -EINVAL;
-	trace_vfs_fsync(file);
-	err = file->f_op->fsync(file, start, end, datasync);
-	trace_vfs_fsync_done(file);
-	return err;
+        #ifdef CONFIG_DYNAMIC_FSYNC
+        if (likely(dyn_fsync_active && !early_suspend_active))
+                return 0;
+        else {
+#endif
+        if (!file->f_op || !file->f_op->fsync)
+                return -EINVAL;
+        return file->f_op->fsync(file, start, end, datasync);
+#ifdef CONFIG_DYNAMIC_FSYNC
+        }
+#endif
 }
 EXPORT_SYMBOL(vfs_fsync_range);
 
@@ -179,6 +190,11 @@ static int do_fsync(unsigned int fd, int datasync)
 
 SYSCALL_DEFINE1(fsync, unsigned int, fd)
 {
+	#ifdef CONFIG_DYNAMIC_FSYNC
+	if (likely(dyn_fsync_active && !early_suspend_active))
+		return 0;
+	else
+	#endif
 	return do_fsync(fd, 0);
 }
 
@@ -199,6 +215,11 @@ EXPORT_SYMBOL(generic_write_sync);
 SYSCALL_DEFINE(sync_file_range)(int fd, loff_t offset, loff_t nbytes,
 				unsigned int flags)
 {
+#ifdef CONFIG_DYNAMIC_FSYNC
+	if (likely(dyn_fsync_active && !early_suspend_active))
+		return 0;
+	else {
+#endif
 	int ret;
 	struct file *file;
 	struct address_space *mapping;
@@ -271,6 +292,9 @@ out_put:
 	fput_light(file, fput_needed);
 out:
 	return ret;
+#ifdef CONFIG_DYNAMIC_FSYNC
+	}
+#endif
 }
 #ifdef CONFIG_HAVE_SYSCALL_WRAPPERS
 asmlinkage long SyS_sync_file_range(long fd, loff_t offset, loff_t nbytes,
@@ -285,6 +309,11 @@ SYSCALL_ALIAS(sys_sync_file_range, SyS_sync_file_range);
 SYSCALL_DEFINE(sync_file_range2)(int fd, unsigned int flags,
 				 loff_t offset, loff_t nbytes)
 {
+#ifdef CONFIG_DYNAMIC_FSYNC
+	if (likely(dyn_fsync_active && !early_suspend_active))
+		return 0;
+	else
+#endif
 	return sys_sync_file_range(fd, offset, nbytes, flags);
 }
 #ifdef CONFIG_HAVE_SYSCALL_WRAPPERS
